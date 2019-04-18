ef07b514b0ea6fd8bfa2d041ab9825fd0a7e272d
Pietro Tenani <pietro.tenani@hotmail.it>
2019-04-18 16:00:58 +0200

    incomplete method

--- src/main/java/actions/effects/ActionResult.java
+++ src/main/java/actions/effects/ActionResult.java
@@ -9,6 +9,7 @@ public class ActionResult {
     public ActionResult(Map<EffectType, Collection<EffectTemplate>> effects,
                         Map<String, Targetable> previousTargets){
         for (EffectType i: EffectType.values()){
+            //TODO: this doesn't work
             if (effects.containsKey(i)) break;
         }
     }
