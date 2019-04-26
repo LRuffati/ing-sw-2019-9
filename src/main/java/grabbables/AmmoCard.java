6f159d294dea911fcf52c8ce91602446bebec760
carme <carmelo.sarta.main@gmail.com>
2019-04-26 09:17:24 +0200

    Tested the class AmmoCard

--- src/main/java/grabbables/AmmoCard.java
+++ src/main/java/grabbables/AmmoCard.java
@@ -31,7 +31,7 @@ public class AmmoCard extends Grabbable {
         ret = "RED:" + amount.get(AmmoColor.RED)
                 +" BLUE:" + amount.get(AmmoColor.BLUE)
                 +" YELLOW:" + amount.get(AmmoColor.YELLOW)
-                +" POWERUP: "+ numOfPowerUp;
+                +" POWERUP:"+ numOfPowerUp;
         return ret;
     }
 
---
+++ src/test/java/grabbables/AmmoCardTest.java
@@ -0,0 +1,53 @@
+package grabbables;
+
+import actions.utils.AmmoAmount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmmoCardTest {
+
+    @BeforeEach
+    void Setup(){
+
+    }
+
+    @Test
+    void constructorTest(){
+        AmmoAmount am = new AmmoAmount();
+        int numOfPu = 1;
+        AmmoCard ac = new AmmoCard(am, numOfPu);
+        assertEquals(am, ac.getAmmoAmount());
+        assertEquals(numOfPu, ac.getNumOfPowerUp());
+    }
+
+    @Test
+    void toStringTest(){
+        AmmoAmount am = new AmmoAmount();
+        int numOfPu = 1;
+        AmmoCard ac = new AmmoCard(am, numOfPu);
+        assertEquals("RED:0 BLUE:0 YELLOW:0 POWERUP:1", ac.toString());
+    }
+
+    //TODO Last two tests to be deleted due to useless overrides.
+    @Test
+    void equalsNullObjectTest(){
+        AmmoAmount am = new AmmoAmount();
+        int numOfPu = 1;
+        AmmoCard ac = new AmmoCard(am, numOfPu);
+        AmmoCard second = null;
+        assertFalse(am.equals(second));
+    }
+
+    @Test
+    void equalsDifferentClassTest(){
+        AmmoAmount am = new AmmoAmount();
+        int numOfPu = 1;
+        AmmoCard ac = new AmmoCard(am, numOfPu);
+        Object second = new ArrayList<>();
+        assertFalse(am.equals(second));
+    }
+}
