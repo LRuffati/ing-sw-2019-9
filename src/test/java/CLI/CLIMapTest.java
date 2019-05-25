5adfbc41d146ec0e38db153c9b064b91f933f7a5
Pietro Tenani <pietro.tenani@hotmail.it>
2019-05-25 14:37:30 +0200

    Fixed a test

--- src/test/java/CLI/CLIMapTest.java
+++ src/test/java/CLI/CLIMapTest.java
@@ -1,11 +1,13 @@
 package CLI;
 
+import board.Coord;
 import board.GameMap;
 import board.Tile;
 import gamemanager.GameBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import player.Actor;
+import uid.DamageableUID;
 import uid.TileUID;
 import viewclasses.GameMapView;
 
@@ -26,7 +28,7 @@ class CLIMapTest {
         String mapPath = "src/resources/map1.txt";
         try {
             builder = new GameBuilder(
-                    mapPath, null, null, tilePath, 3);
+                    mapPath, null, null, tilePath, 5);
         }
         catch (FileNotFoundException ignored){
         }
@@ -41,6 +43,9 @@ class CLIMapTest {
         TileUID t = (TileUID) iterator.next();
         actorList.get(0).move(t);
         */
+        for(DamageableUID damageableUID : map.getDamageable()){
+            map.getPawn(damageableUID).getActor().move(map.getPosition(new Coord(1,2)));
+        }
         gmv = map.generateView(actorList.get(0).getPawn().getDamageableUID());
         CLIMap map = new CLIMap(gmv);
         map.printMap();
