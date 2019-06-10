7d505ec4735f2de4b38fd1058634d89b1c3e1432
pietro <pietr.tenani@hotmail.it>
2019-06-10 16:19:10 +0200

    Modified Message class

--- src/main/java/testcontroller/Message.java
+++ src/main/java/testcontroller/Message.java
@@ -1,46 +1,15 @@
 package testcontroller;
 
-import actions.effects.Effect;
-import board.GameMap;
-import board.Sandbox;
-import testcontroller.controllerstates.UpdateTypes;
-import viewclasses.GameMapView;
-
-import java.util.List;
-
 /**
  * This message represents a status update from the server to the client
  */
 public interface Message {
 
-    /**
-     * This is the type of the change, it'll change the way I interpret the data
-     * @return
-     */
-    UpdateTypes type();
 
-    /**
-     * A human readable description of the changes
-     * @return
-     * TODO: Change to some other format if necessary
-     */
-    String message();
 
     /**
      * I communicate the changes that happened
      * @return
      */
     List<EffectView> getChanges();
-
-    /**
-     *
-     * @return
-     */
-    Sandbox sandbox();
-
-    /**
-     *
-     * @return
-     */
-    Integer gameMapIteration();
 }
