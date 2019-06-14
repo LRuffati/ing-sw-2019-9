e72cb7a2376f2b3a959bc2249a8b61cd8dcf8cf9
pietro <pietro.tenani@hotmail.it>
2019-06-14 19:24:08 +0200

    OcjectMap checks if valid choice

--- src/main/java/network/ObjectMap.java
+++ src/main/java/network/ObjectMap.java
@@ -1,12 +1,10 @@
 package network;
 
 
-import board.GameMap;
-import board.Sandbox;
+import testcontroller.ChoiceBoard;
 import testcontroller.controllerclient.ControllerMessageClient;
 import testcontroller.controllermessage.ControllerMessage;
 import testcontroller.controllerstates.SlaveControllerState;
-import viewclasses.GameMapView;
 
 import java.rmi.server.UID;
 import java.util.HashMap;
@@ -29,10 +27,27 @@ public class ObjectMap {
         choiceMap.clear();
     }
 
+
+    private boolean checkPick(ControllerMessage controllerMessage, List<Integer> choices) {
+        ChoiceBoard choiceBoard = controllerMessage.genView();
+        if(choices == null) return false;
+        if(!choiceBoard.optional && choices.isEmpty()) return false;
+        if(choiceBoard.single && choices.size() > 1) return false;
+        int max = 0;
+        if(choiceBoard.stringViews != null) max = choiceBoard.stringViews.size();
+        if(choiceBoard.powerUpViews != null) max = choiceBoard.powerUpViews.size();
+        if(choiceBoard.targetViews!= null) max = choiceBoard.targetViews.size();
+        if(choiceBoard.weaponViews != null) max = choiceBoard.weaponViews.size();
+        if(choiceBoard.actionViews != null) max = choiceBoard.actionViews.size();
+        for(int choice : choices)
+            if(choice < 0 || choice >= max)
+                return false;
+        return true;
+    }
+
     private String newID(){
         return new UID().toString();
     }
-
 
     private ControllerMessage handlePick(ControllerMessage controllerMessage) {
         String id = newID();
@@ -45,11 +60,14 @@ public class ObjectMap {
     }
 
     public ControllerMessage pick(String token, String choiceId, List<Integer> choices) {
-
         if(!choiceMap.containsKey(choiceId)) {
             return new ControllerMessageClient(Database.get().getControllerByToken(token).getInstruction(), null);
         }
-        return handlePick(choiceMap.get(choiceId).pick(choices));
+        ControllerMessage message = choiceMap.get(choiceId);
+        if(checkPick(message, choices))
+            return handlePick(choiceMap.get(choiceId).pick(choices));
+        else
+            return message;
     }
 
     public ControllerMessage init(ControllerMessage controllerMessage) {
