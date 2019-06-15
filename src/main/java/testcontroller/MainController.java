73d90d21d934caab65027c6ea4335f8a9bf4c95e
Lorenzo Ruffati <lruffati@gmail.com>
2019-06-15 11:43:10 +0200

    Fill MainController functions for endturn and respawn

--- src/main/java/testcontroller/MainController.java
+++ src/main/java/testcontroller/MainController.java
@@ -112,13 +112,12 @@ public class MainController {
             slaveController.onTimer(ms);
     }
 
-    private void terminateGame(){
-        //todo
-    }
-
     private void startGame() {
         gameStarted = true;
         createGame();
+        for (SlaveController i: slaveControllerList){
+            slaveMap.put(i.getSelf().pawnID(), i);
+        }
         notifyStarting(game.getMapName());
         //todo notify inizio gioco
     }
@@ -202,7 +201,17 @@ public class MainController {
      *
      * @param lastPlayed the player whose turn just ended
      */
-    public void endTurn(Player lastPlayed){
+    public void endTurn(Actor lastPlayed){
+        boolean frenzyBefore = scoreboard.finalFrenzy();
+        SlaveController current = slaveMap.get(lastPlayed.pawnID());
+        SlaveController next;
+        int currIndex = slaveControllerList.indexOf(current);
+        int size = slaveControllerList.size();
+        if (currIndex<(size-1)){
+            next = slaveControllerList.get(currIndex+1);
+        } else {
+            next = slaveControllerList.get(0);
+        }
         /*
         0. Riempie la mappa
         1. Prende tutti i domination points
@@ -213,13 +222,90 @@ public class MainController {
         5. Alla fine il thread controlla con Scoreboard che non sia iniziata la frenesia finale
             5a. Frenesia iniziata: Chiama Main.finalFrenzy(nextPlayer)
             5b. Inizia il main line sul prossimo turno
-         */
+        */
+        List<DamageableUID> dead = new ArrayList<>();
+        for (TileUID t: getGameMap().allTiles()){
+            getGameMap().getTile(t).endTurn(lastPlayed);
+        }
+        for (DamageableUID uid: getGameMap().getDamageable()){
+            if (getGameMap().getPawn(uid).getActor().endTurn()){
+                dead.add(uid);
+            }
+        }
+
+        if (scoreboard.finalFrenzy() & current.getSelf().isLastInFrenzy()){
+            // It's not necessary to respawn if the game ends anyway
+            this.endGame();
+            return;
+        }
+
+        if (!frenzyBefore & scoreboard.finalFrenzy()){ // If something triggered the FF
+            //1. Start finalFrenzy for each pawn
+            boolean afterLast = false;
+            for (SlaveController i: slaveControllerList){
+                // after last is false for all players between the first one and the one who just
+                // ended the turn and true for all others
+                i.getSelf().beginFF(!afterLast);
+                afterLast = afterLast | i.equals(current);
+            }
+            //2. For current
+            current.getSelf().setLastInFrenzy();
+        }
+
+        this.startRespawn(dead, 1, next::startMainAction); // LAST LINE OF THE FUNCTION
+        return;
     }
 
-    public void finalFrenzy(Player next){
+    /**
+     *
+     * @param dead the players which have to be resurrected
+     * @param cards the amount of cards to draw. 1 for respawn 2 for first placement
+     * @param onAllRespawned The runnable to call when the function is called with an empty list.
+     *                      Main use is for it to trigger the "start next turn". It should be not
+     *                      blocking, it will not be run in a thread but just called
+     */
+    private void startRespawn(List<DamageableUID> dead, int cards, Runnable onAllRespawned) {
         /*
-        1. Chiama su tutti gli actor: finalFrenzyBegin
+        1. Add the given amount of cards to the first one (drawPowerupsraw)
+        2. In a thread call respawn on the first slaveController
+            a. Provide a consumer that:
+                - Discards the powerup
+                - Moves pawn to the corresponding tile
+                - Calls startRespawn with the tail of dead and same parameter
+        3. If dead is empty run onAllRespawned
+         */
+        if (dead.isEmpty())
+            onAllRespawned.run();
 
+        DamageableUID head = dead.get(0);
+        List<DamageableUID> tail = dead.subList(1,dead.size());
+
+        slaveMap.get(head).getSelf().drawPowerupRaw(cards);
+
+        Consumer<PowerUp> onRespawned =
+                powerUp -> {
+                    SlaveController slave = slaveMap.get(head);
+                    Actor respawnedActor = slave.getSelf();
+                    Set<Tile> spawns =
+                            getGameMap().allTiles().stream()
+                                    .map(getGameMap()::getTile)
+                                    .filter(Tile::spawnPoint)
+                                    .collect(Collectors.toSet());
+                    TileUID destination = powerUp.spawnLocation(spawns);
+                    respawnedActor.discardPowerup(powerUp);
+                    respawnedActor.getPawn().move(destination);
+                    startRespawn(tail, cards, onAllRespawned);
+                };
+        slaveMap.get(head).startRespawn(onRespawned);
+    }
+
+    /**
+     * Called to count
+     */
+    void endGame(){
+        /*
+            1. Count points
+            2. Declare winner
          */
 
     }
