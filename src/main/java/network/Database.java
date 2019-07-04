75348bdd40ece263009f1012bf92a096fc146187
pietro <pietro.tenani@hotmail.it>
2019-07-04 11:46:00 +0200

    Solved a nice bug

--- src/main/java/network/Database.java
+++ src/main/java/network/Database.java
@@ -127,7 +127,7 @@ public class Database {
             usersByToken.put(token, user);
 
             try {
-                network.setToken(token);
+                proxy.setToken(token);
             }
             catch (RemoteException e) {
                 logger.log(Level.INFO, "login-SetToken", e);
@@ -185,6 +185,7 @@ public class Database {
             logout(token);
         }
 
+        controllerByToken.put(token, mainController.bind(getUserByToken(token), proxy));
         networkByToken.put(token, proxy);
         disconnectedToken.remove(token);
 
